75aa91f4be1a036c4652749eea40a5c9b00bf1ee
sUDERrOADSIDE <35688749+sUDERrOADSIDE@users.noreply.github.com>
2018-03-06 13:24:43 +0530

    Update 123.c

--- 123.c
+++ 123.c
@@ -3,9 +3,9 @@
 int main(void) 
 {
 	int n1,n2,sum;
-	printf("\n enter the first number:");
+	printf("\n enter first number:");
 	scanf("%d",&n1);
-	printf("\n enter the second number:");
+	printf("\n enter  second number:");
 	scanf("%d",&n2);
 	sum=n1+n2;
 	printf("%d",sum);
